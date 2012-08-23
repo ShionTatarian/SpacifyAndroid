959c447e3785c56e19ac0f91a48c3d79503b4dd8
Tommy <tommy.t.eklund@gmail.com>
2012-08-23 23:47:35 +0300

    Moved WorkService to separate library project.

--- Spacify/src/fi/android/spacify/SpacifyApplication.java
+++ Spacify/src/fi/android/spacify/SpacifyApplication.java
@@ -1,6 +1,7 @@
 package fi.android.spacify;
 
 import android.app.Application;
+import fi.android.service.web.WebService;
 import fi.android.spacify.service.WorkService;
 
 public class SpacifyApplication extends Application {
@@ -8,6 +9,7 @@ public class SpacifyApplication extends Application {
 	@Override
 	public void onCreate() {
 		WorkService.init(5);
+		WebService.init(this);
 		
 		super.onCreate();
 	}
